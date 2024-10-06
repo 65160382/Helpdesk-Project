277243cbe04889c6d5063900769cd378f266e586
Piyawat Seepattha <65160382@go.buu.ac.th>
2024-10-06 14:28:17 +0700

    ticket.js

--- routes/myrouters.js
+++ routes/myrouters.js
@@ -2,9 +2,24 @@
 const express = require('express');
 const router = express.Router();
 
+let tickets = [];
 
 router.get('/', (req, res) => {
     res.render('index');
 });
+
+// Route เพื่อรับข้อมูลจากฟอร์ม
+router.post('/submit-ticket', (req, res) => {
+    const { title, category, name, priority, description } = req.body;
+    const newTicket = { title, category, name, priority, description };
+    tickets.push(newTicket);
+    res.redirect('/tickets');
+});
+
+// Route เพื่อแสดงข้อมูลในตาราง
+router.get('/tickets', (req, res) => {
+    res.render('tickets', { tickets });
+});
+
 
 module.exports = router;
