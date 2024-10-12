105d0c93ead13069d2c2dbe8147fc2781d6027a2
Piyawat Seepattha <65160382@go.buu.ac.th>
2024-10-13 00:26:18 +0700

    admin authorize

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,20 @@
-const bcrypt = require('bcryptjs')
 const User = require('../models/userModel');
 
 exports.getAllUsers = async (req, res) => {
-    try {
+  try {
       const users = await User.findAll();
+      const user = req.session.user;
       
-    //   // Bcrypt passwords before sending to EJS
-    //   users.forEach(user => {
-    //     user.password = bcrypt.hashSync(user.password, 10);
-    //   });
-  
-      res.render('user', { users });
-    } catch (err) {
+      const adminUsername = 'admin'; // Define the admin username
+      
+      if (user && user.username === adminUsername) {
+          res.render('user', { users });
+      } else {
+          console.log('Unauthorized access attempt');
+          res.status(403).send('Unauthorized');
+      }
+  } catch (err) {
       console.error(err);
       res.status(500).send('Server Error');
-    }
-  };
+  }
+};
